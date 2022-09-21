17e22a79886bd15556b11c0cc6de3c5afe0b4c22
MiekeSlim <74954820+MiekeSlim@users.noreply.github.com>
2022-09-21 10:13:52 +0200

    Add files via upload

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,28 +1,24 @@
 PennController.ResetPrefix(null) // Shorten command names (keep this)
 PennController.DebugOff() // Don't show the debug window
 
-// Resources are hosted on a distant server
+// The resources are hosted on a distant server. NOTE: If possible, it is recommended to store your resources in ZIP files (for information see: https://doc.pcibex.net/how-to-guides/managing-resources/), unfortunately, my university's servers do not allow this.
 AddHost("https://users.ugent.be/~mslim/VW_DWR_Stimuli/images/");
 
 // PHP script that receives, stores (and will also output) the eye-tracking data
 //EyeTrackerURL("PUT YOUR OWN URL HERE")
 
+// We don't want to show the progress bar in this experiment. We can hide the progress bar (which is shown by default) with the following code:
+var showProgressBar = false;
+
 // Sequence that determines the order of the elements in this experiment
 Sequence("Checks", "Welcome", "Consent", "ProlificID_trial", "WebcamSetUp", "CalibrationSetUp", "Instructions", randomize("Trials"), "QuestionnairePage", "Send", "Final")
 
-// Check for L1 (if the participant indicates that English is not their L1, they won't able to continue to the experiment)
+// Pre-experimental checks:
 PennController("Checks",
-    newImage("logo", "logo_UGent_EN_RGB_2400_color.png")
-        .size("10vw")       
-        .print("20vw","00vh")
-    ,
-    newImage("logo2", "icon_UGent_PP_EN_RGB_2400_color.png")
-        .size("20vw")       
-        .print("55vw","2vh")                           
-    ,           
+    // Does the participant consent that we will use their webcam?
     newText("Consent", "Two short questions before we begin: <br><br> We will use your webcam to collect data on where you are looking on the screen. We will <b> not </b> collect any video data or any other type of data that may reveal your identity. Do you give us permission to use your webcam?<br><br>")
     ,
-    newCanvas( "myCanvas", "60vw" , "20vh")
+    newCanvas( "ConsentCanvas", "60vw" , "20vh")
         .settings.add(0,0, getText("Consent"))
         .print("20vw", "20vh")
     ,    
@@ -39,17 +35,19 @@ PennController("Checks",
     getSelector("yesnoConsent")
         .settings.log()
         .test.selected(getButton("yesConsent") )
+        // If they press 'no', they are instructed that they cannot participate. If they press 'yes', the experiment continues.
         .failure(
             newCanvas("NoPermision", "60vw" , "20vh").settings.add(0,0, newText("<br><br>Unfortunately you cannot participate in this study. Please close the experiment by closing the browser (you can ignore possible pop-up screens) <br><br>"))
                 .print("20vw", "32vh")
             ,
             newButton("waitforever")
-                .wait()
+                .wait() // The button is not printed, so they're stuck on this page.
         )         
-    ,          
+    ,    
+    // Does the participant use Google Chrome?   
     newText("Chrome", "This study only works well if you are using the Google Chrome browser on a laptop or desktop computer (so not on a mobile phone or tablet). Are you currently using <b> Google Chrome Desktop </b>? <br><br>")
     ,
-    newCanvas( "myCanvas", "60vw" , "20vh")
+    newCanvas( "ChromeCanvas", "60vw" , "20vh")
         .settings.add(0,0, getText("Chrome"))
         .print("20vw", "40vh")
     ,    
@@ -68,25 +66,17 @@ PennController("Checks",
         .settings.log()
         .test.selected(getButton("yesChrome") )
         .failure(
+            // If they press 'no', they are instructed that they cannot participate. If they press 'yes', the experiment continues.
             newCanvas("WrongBrowser", "60vw" , "20vh").settings.add(0,0, newText("<br><br>Unfortunately, this experiment only works on Google Chrome (which can be downloaded for free). Please close the experiment by closing the browser (you may ignore possible pop-up screens), and come back on Chrome.<br><br>"))
                 .print("20vw" , "50vh")
             ,
             newButton("waitforever")
-                .wait()
+                .wait() // The button is not printed, so they're stuck on this page.
         )      
 )
-.setOption("hideProgressBar", true) 
 
 // Welcome page
 PennController("Welcome",
-    newImage("logo", "logo_UGent_EN_RGB_2400_color.png")
-        .size("10vw")       
-        .print("20vw","00vh")
-    ,
-    newImage("logo2", "icon_UGent_PP_EN_RGB_2400_color.png")
-        .size("20vw")       
-        .print("55vw","2vh")                                         
-    ,        
     newText("WelcomeText", "<p>Welcome and thank you for participating in this experiment! </p><p> </p><p> Cognitive scientists often record eye movements to study human behaviour, because eye movements tell a lot about how we divide our attention and how we make decisions. Typically, cognitive scientists use expensive eye-tracking devices to record where people are looking on a computer screen. The aim of this experiment is to test whether we could also use webcams to achieve this goal. </p><p> </p><p> The task is very simple, and should take you roughly 5-10 minutes to complete: A cross (+) will appear at various positions on your screen. Please look closely at each cross that appears, untill it disappears. We will <b> not </b> collect any video data or any other type of data that may reveal your identity: We only collect data on where on the screen your eyes are looking during the experiment. <br> <br>  Because we will use your webcam to follow your eye movements during this task, it is important that you are in a well-lit and quiet environment. Please turn off your mobile phone or other devices that may distract you during this task. Also, please close other websites that you may have open.</p> <p> If you have any questions about this experiment, feel free to get in touch with me (Mieke Slim) via email: mieke.slim@ugent.be</p>")
     ,           
     newCanvas( "myCanvas", "60vw" , "60vh")
@@ -98,18 +88,9 @@ PennController("Welcome",
         .print("center at 50vw", "80vh")
         .wait()
 )
-.setOption("hideProgressBar", true) 
 
-// Consent page
+// Consent page. You can replace this with your own. 
 PennController("Consent",
-    newImage("logo", "logo_UGent_EN_RGB_2400_color.png")
-        .size("10vw")       
-        .print("20vw","00vh")
-    ,
-    newImage("logo2", "icon_UGent_PP_EN_RGB_2400_color.png")
-        .size("20vw")       
-        .print("55vw","2vh")                           
-    ,               
     newText("ConsentText", "<p>This experiment has been approved by the Ethical Comittee from the Faculty of Psychology and Educational Sciences at Ghent University. We request your consent for participation in this experiment. Therefore, please read the following carefully: </p > <p>I declare that I, as a participant in a research project in the Department of Experimental Psychology at Ghent University:<br><br> <ol> <li> have been informed about the research objectives, the questions and the tasks that I will encounter during the research and that I was given the opportunity to receive further information if desired<br><br> </li><li> will participate out of free will in the research project <br><br> </li><li> am aware that the researchers do not collect any personal information that may be used to identify my identity (such as video recordings). All the data that will be collected is completely anonymized; <br><br> </li><li> give informed consent to the researchers to store, process, and report my data in anonymized form <br><br> </li><li> am aware of the option to stop my participation in this research at any moment in time without having to provide a reason; <br><br> </li><li> know that participating or stopping my participation in the research has no negative consequences of any kind for me (apart from not receiving my payment via Prolific) <br><br> </li><li> am aware of the option to ask the researcher(s) for a summary of the results after the study is finished and the results have been known; <br><br> </li><li> agree that my data may be used for further analysis by other researchers after complete anonymization; <br><br> </li><li> am aware that Ghent University is the responsible entity with regards to the personal information collected during the study. I am also aware that the data protection officer can give me more information about the protection of my personal information. Contact: Hanne Elsen (privacy@ugent.be).</li> </ol> <br>In case you give your informed consent to participate in this study, please click on the button below. If you do not give your informed consent, please close this experiment. </p>")
     ,
     newCanvas( "myCanvas", "60vw" , "60vh")
@@ -121,46 +102,9 @@ PennController("Consent",
             .print("center at 50vw", "95vh")
             .wait()
 )
-.setOption("hideProgressBar", true) 
-
-//Ask for Prolific ID
-PennController("ProlificID_trial",
-    newImage("logo", "logo_UGent_EN_RGB_2400_color.png")
-        .size("10vw")       
-        .print("20vw","00vh")
-    ,
-    newImage("logo2", "icon_UGent_PP_EN_RGB_2400_color.png")
-        .size("20vw")       
-        .print("55vw","2vh")                           
-    ,    
-    defaultText
-        .print()
-    ,
-    newText("<p>Please fill in your Prolific ID below, so we can process your payment</p>")
-    ,
-    newTextInput("ProlificID")
-        .print()
-    ,
-    newButton("Continue")
-        .print()
-        .wait()
-    ,
-    newVar("ProlificID")
-        .settings.global()
-        .set( getTextInput("ProlificID") )
-    )
-    .log( "ProlificID" , getVar("ProlificID") )
 
 //Webcam set-up and calibration
 newTrial("WebcamSetUp",
-    newImage("logo", "logo_UGent_EN_RGB_2400_color.png")
-        .size("10vw")       
-        .print("20vw","00vh")
-    ,
-    newImage("logo2", "icon_UGent_PP_EN_RGB_2400_color.png")
-        .size("20vw")       
-        .print("55vw","2vh")                           
-    ,               
     newText("WebcamInstructions", "<p> Before the task begins, we need to calibrate your webcam so the experiment can follow your eye movements. On the next page, a calibration procedure will start. First, you will see the webcam recording on the top left corner of your screen. Please make sure your face is fully visible, and that you sit centrally in front of your webcam by following these instructions:.</p> <p> The next pages will appear in fullscreen. <b> Please do not close the fullscreen for the remainder of this experiment </b></p>")
     ,
     newImage("Instructions", "Instructions.png")
@@ -168,7 +112,7 @@ newTrial("WebcamSetUp",
     ,
     newCanvas("myCanvas", "60vw" , "60vh")
         .settings.add(0,0, getText("WebcamInstructions"))
-        .settings.add(0,"10vh", getImage("Instructions"))
+        .settings.add(0,"center at 50%", getImage("Instructions"))
         .print()
     ,
     newButton("Take me to the next page (which will appear in fullscreen)")
@@ -178,11 +122,10 @@ newTrial("WebcamSetUp",
     ,
     fullscreen()
 )
-.setOption("hideProgressBar", true) 
 
-// Calibration page
+// In this section, we prompt the calibration procedure. NOTE: The calibration procedure is different than described than in the paper, because the PCIbex developers have updated this procedure since then. The functioning of the calibration procedure is still more-or-less the same. 
 newTrial("CalibrationSetUp",
-    newText("CalibrationInstructions", "<p>In the calibration procedure, you will see eight buttons on your screen. Please click on all these buttons and follow your cursor closely with your eyes. <br><br> Once you've clicked on all buttons, a new button will appear in the middle of the screen. Please click on this button and look at the centre of your screen for three seconds. In these three seconds, the webcam eyetracker checks whether it's well calibrated.</p> <p> In case calibration fails, the procedure will be repeated. </p>")
+    newText("CalibrationInstructions", "<p>In the calibration procedure, you will see nine circles appear on your screen. Please look at these circles closely as they appear untill they disappear. <br><br> At the end, a new circle will appear in the middle of the screen. Please look at this circle for three seconds. In these three seconds, the webcam eyetracker checks whether it's well calibrated.</p> <p> In case calibration fails, the procedure will be repeated. </p>")
     ,
     newCanvas("myCanvas", "60vw" , "60vh")
         .settings.add(0,0, getText("CalibrationInstructions"))
@@ -194,12 +137,11 @@ newTrial("CalibrationSetUp",
         .wait( newEyeTracker("tracker").test.ready() )
         .remove()
     ,
-    getEyeTracker("tracker").calibrate(5)
+    getEyeTracker("tracker").calibrate(5) // '5' is the calibration treshol
         .log()
 )
-.setOption("hideProgressBar", true) 
 
-// Experiment instructions
+// Present the task instructions.
 newTrial("Instructions", 
     newText("TaskInstructions", "<p>You're all set to start the experiment! The task is very simple: Please look closely at all crosses that appear on the screen untill they disappear. <br> <br> You don't have to do anything else during this task, but every now and then a button will appear in the middle of your screen. Click on this button and look at the center of your screen for three seconds. The webcam will check whether it is still calibrated. If it is, the next trial will automatically start. Otherwise, the calibration procedure will be repeated. <br><br> The task should take roughly 5 minutes.</p>")
     ,
@@ -216,7 +158,7 @@ newTrial("Instructions",
 // Start the trials of the experiment
 PennController.Template("FixationTrials.csv",
     row => PennController("Trials", 
-        // The callback commands lets us log the X and Y coordinates of the estimated gaze-locations at each recorded moment in time (Thanks to Jeremy Zehr for helping us construct this command)
+        // The callback commands lets us log the X and Y coordinates of the estimated gaze-locations at each recorded moment in time (Thanks to Jeremy Zehr for writing this function)
             newEyeTracker("tracker",1).callback( function (x,y) {
               if (this != getEyeTracker("tracker")._element.elements[0]) return;
               getEyeTracker("tracker")._element.counts._Xs.push(x);
@@ -276,7 +218,7 @@ PennController.Template("FixationTrials.csv",
             .wait()
         ,
         // Canvas that contains the target stimulus                  
-        newCanvas("trackedCanvas", "15vh", "15vh") 
+        newCanvas("trackedCanvas", "10vh", "10vh") 
             .add( "center at 50%" , "middle at 50%" , getText("fixationText").css("font-size", "15vh"))
             .print(row.X_position, row.Y_position)
         ,
@@ -307,8 +249,7 @@ PennController.Template("FixationTrials.csv",
         ,
         getVar("trialsLeftbeforeCalibration")
             .set( v => v-1)           
-    )
-    .setOption("hideProgressBar", true)          
+    )   
     .log("Position", row.Position)
     .log("Xpos.absolute", row.X_position) // X position measured in pixels
     .log("Ypos.absolute", row.Y_position) // Y position measured in pixess
@@ -331,14 +272,6 @@ PennController("QuestionnairePage",
        });
     }).call()
     ,                  
-    newImage("logo", "logo_UGent_EN_RGB_2400_color.png")
-        .size("10vw")       
-        .print("20vw","0vh")
-    ,
-    newImage("logo2", "icon_UGent_PP_EN_RGB_2400_color.png")
-        .size("20vw")       
-        .print("55vw","2vh")                           
-    ,                
     newHtml("Questionnaire", "Questionnaire.html")
         .settings.log()
         .print("30vw","15vh")
@@ -349,8 +282,7 @@ PennController("QuestionnairePage",
             getHtml("Questionnaire").test.complete()
                 .failure( getHtml("Questionnaire").warn() )
         )                      
-              )
-    .setOption("hideProgressBar", true)          
+              )       
 
 // Send the results to the server
 PennController.SendResults("Send");
@@ -359,14 +291,6 @@ PennController.SendResults("Send");
 newTrial("Final",    
     exitFullscreen()
     ,
-    newImage("logo", "logo_UGent_EN_RGB_2400_color.png")
-        .size("10vw")       
-        .print("20vw","00vh")
-    ,
-    newImage("logo2", "icon_UGent_PP_EN_RGB_2400_color.png")
-        .size("20vw")       
-        .print("55vw","2vh")                           
-    ,       
     newText("Final","This is the end of the experiment. <strong> Please verify your participation on Prolific by clicking on this link: <p><a href='https://app.prolific.co/submissions/complete?cc=66C714D9'>https://app.prolific.co/submissions/complete?cc=66C714D9</a></p> </strong> <br> Thank you for your participation! If you have any questions or if you want to know more about the results, please get in touch with me via mieke.slim@ugent.be")
     ,
     newCanvas("myCanvas", "60vw" , "60vh")
@@ -375,4 +299,3 @@ newTrial("Final",
     ,     
     newButton("waitforever").wait() // Not printed: wait on this page forever
 )
-    .setOption("hideProgressBar", true)
